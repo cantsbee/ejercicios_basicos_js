a95c252aa7dc2017a60924fc2acfa40fdc28f822
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2024-11-11 12:11:34 +0100

    Add files via upload

---
+++ Ejercicio2.js
@@ -0,0 +1,28 @@
+//1. Luke Skywalker cumple años:
+const jedi = {nombre: "Luke Skywalker", edad: 25};
+
+//2. Presentación al estilo Leia Organa:
+
+let nombre: "Leia";
+
+let apellido: "Organa";
+
+let edad: 20;
+
+console.log("Soy " + nombre + apellido + "," + " tengo " + edad + " años y soy una princesa de Alderaan." );
+
+//3.Calculando el coste total de sables de luz:
+
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+const suma = {precio.sable1 + precio.sable2}; 
+
+console.log(suma);
+
+//4. Actualizando el precio final de las naves:
+
+//- Modifica el valor de la variable global precioBaseGlobal a 25.000 créditos.
+
+//- Actualiza el precio final (precioFinal) de dos naves ("Ala-X" y "Halcón Milenario") sumando el valor de precioBaseGlobal a su precio base (precioBase).
+
---
+++ ejercicio3.js
@@ -0,0 +1,10 @@
+//3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+let num = 5;
+let num2= 10;
+ console.log(num*num2);
+//3.2 Divide 10 por 2 y muestra el resultado en un console.
+
+
+//3.3 Muestra mediante un console el resto de dividir 15 por 9.
+//3.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+//3.5 Usa el correcto operador de asignación que resultará en i = 50, teniendo dos variables c = 10 y m = 5.
