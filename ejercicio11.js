b24ab2749ca6a0bf3a64d7003b2b80767f7fdc50
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-04-18 18:46:36 +0200

    Update ejercicio11.js

--- ejercicio11.js
+++ ejercicio11.js
@@ -5,6 +5,21 @@ Es un poco locura, pero podremos ejercitar nuestra lógica con este ejercicio.
 Puedes usar este array para probar tu función:
 
 const mixedElements = [6, 1, 'Marvel', 1, 'hamburguesa', '10', 'Prometeo', 8, 'Hola mundo'];
+const mixedElements = [6, 1, 'Marvel', 1, 'hamburguesa', '10', 'Prometeo', 8, 'Hola mundo'];
+
 function averageWord(list) {
-  // Completar
+  let total = 0;
+
+  for (let element of list) {
+    if (typeof element === 'number') {
+      total += element; // 
+    } else if (typeof element === 'string') {
+      total += element.length; 
+    }
+  }
+
+  return total / list.length; 
 }
+
+console.log(averageWord(mixedElements)); 
+
