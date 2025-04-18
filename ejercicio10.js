5507741e8d01b564ae8ecdb5c4407293f055f94d
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-04-18 18:51:15 +0200

    Update ejercicio10.js

--- ejercicio10.js
+++ ejercicio10.js
@@ -3,6 +3,17 @@ Calcular un promedio es una tarea extremadamente común, así que prueba a imple
 Puedes usar este array para probarla:
 
 const numbers = [12, 21, 38, 5, 45, 37, 6];
+
 function average(numberList) {
-  // Completar
+  if (numberList.length === 0) return 0; // Evita división por cero
+
+  let sum = 0;
+
+  for (let num of numberList) {
+    sum += num;
+  }
+
+  return sum / numberList.length;
 }
+
+console.log(average(numbers));
