b30fba9439e4ff6bf23dd9c0b0559e6093ba8208
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-04-25 16:04:01 +0200

    Update ejercicio23.js

--- ejercicio23.js
+++ ejercicio23.js
@@ -11,8 +11,27 @@ const movies = [
   { name: "The Lord of the Rings", durationInMinutes: 967 },
   { name: "Star Wars: A New Hope", durationInMinutes: 214 },
   { name: "Terminator", durationInMinutes: 140 },
-  { name: "Spirited Away", durationInMinutes: 80 },  
-  { name: "The Matrix", durationInMinutes: 136 },  
-  { name: "Amélie", durationInMinutes: 110 },  
+  { name: "Spirited Away", durationInMinutes: 80 },
+  { name: "The Matrix", durationInMinutes: 136 },
+  { name: "Amélie", durationInMinutes: 110 },
   { name: "Eternal Sunshine of the Spotless Mind", durationInMinutes: 108 },
 ];
+
+const smallMovies = [];
+const mediumMovies = [];
+const largeMovies = [];
+
+for (const movie of movies) {
+  if (movie.durationInMinutes < 100) {
+    smallMovies.push(movie);
+  } else if (movie.durationInMinutes >= 100 && movie.durationInMinutes < 200) {
+    mediumMovies.push(movie);
+  } else if (movie.durationInMinutes >= 200) {
+    largeMovies.push(movie);
+  }
+}
+
+console.log("Películas pequeñas:", smallMovies);
+console.log("Películas medianas:", mediumMovies);
+console.log("Películas grandes:", largeMovies);
+
