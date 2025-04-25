193e8b6bcd2fc8e091244987e96d72c4ae362808
Juanita Zuluaga Acosta <59172512+cantsbee@users.noreply.github.com>
2025-04-25 15:54:51 +0200

    Update ejercicio17.js

--- ejercicio17.js
+++ ejercicio17.js
@@ -5,8 +5,13 @@ Debería tener este formato cada uno de los console.log: "La propiedad ____ tien
 Puedes usar este objeto:
 
 const alien = {
-    name: 'Xenomorph',
-    species: 'Xenomorph XX121',
-    origin: 'Unknown',
-    weight: 180
+  name: 'Xenomorph',
+  species: 'Xenomorph XX121',
+  origin: 'Unknown',
+  weight: 180
 };
+
+for (const property in alien) {
+  console.log(`La propiedad ${property} tiene cómo valor: ${alien[property]}`);
+}
+
